d2865ae4838fb479434d7ff68b5e3cdd8d852453
CesiliaMaria <cesilia.maria05@gmail.com>
2025-06-11 20:23:29 +0700

    auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,4 +3,9 @@ const express = require("express");
 const router = express.Router();
 
 //Auth Routes
-router.post("/register", registerUser);
+router.post("/register", registerUser); // Register User
+router.post("/login", loginUser); //Login user
+router.get("/profile", protect, getUserProfile); // Get user profile
+router.put("/profile", protect, updateUserProfile); //Update profile
+
+module.exports = router;
