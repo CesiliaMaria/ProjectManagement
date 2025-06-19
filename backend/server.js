3fa9bfdaf4d90d65078e6c5ee48d70c119655a12
CesiliaMaria <cesilia.maria05@gmail.com>
2025-06-19 23:49:31 +0700

    Postman, mongodb & server js fix

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const reportRoutes = require("./routes/reportRoutes");
 
 const app = express();
 
-//Middleware buat handle CORS
+// Middleware buat handle CORS
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "*",
@@ -23,18 +23,15 @@ app.use(
 // Connect Database
 connectDB();
 
-//Middleware
+// Middleware
 app.use(express.json());
 
-// Connect Database
-connectDB();
+//ROUTES
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/reports", reportRoutes);
 
-//Routes
-app.use("api/auth", authRoutes);
-app.use("api/users", userRoutes);
-app.use("api/tasks", taskRoutes);
-app.use("api/reports", reportRoutes);
-
-//Start Server
+// Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
